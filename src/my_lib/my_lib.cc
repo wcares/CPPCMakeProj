07af9422b8a88afa8cc5b80e5f60a3c881aeb92d
Wcares <wcares563@gmail.com>
2022-11-16 07:50:23 +0545

    added more libraries using Conan & configured unit test

--- src/my_lib/my_lib.cc
+++ src/my_lib/my_lib.cc
@@ -2,6 +2,10 @@
 using namespace std;
 #include "my_lib.h"
 
+// unsigned int Factorial( unsigned int number ) {
+//     return number <= 1 ? number : Factorial(number-1)*number;
+// }
+
 void add(int a, int b)
 {
     cout << "this console out is from library source" << endl;
--- src/my_lib/my_lib.h
+++ src/my_lib/my_lib.h
@@ -8,6 +8,6 @@
  * @details Here more stuff just to check out ayee Owner: Immortal Beings
  * @return description of the return value(some extra stuff here)
  */
-
+// unsigned int Factorial( unsigned int number );
 
 void add(int a, int b);
